4f51fffb0dc636595602f66b0b5cbf803bc6b11b
Andy Lindsay <andydlindsay@gmail.com>
2022-03-08 12:44:36 -0800

    add problem statement to problem.js

--- w01d02/problem.js
+++ w01d02/problem.js
@@ -1,3 +1,6 @@
+// ### Problem:
+// Write a program that takes in an unlimited number of command line arguments and prints out the sum of them. If any argument is not a whole number, skip it. Do not support negative numbers.
+
 const commandLineArguments = process.argv;
 
 // console.log(commandLineArguments);
